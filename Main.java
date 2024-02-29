1dd9cc77dd06e08e50300e64e87e103e2b5bdb0d
blancadg <161023691+blancadg@users.noreply.github.com>
2024-02-29 10:13:19 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package cuentas;
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        operativa_cuenta(cuenta1, null) = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+    private static CCuenta operativa_cuenta(CCuenta cuenta1, Float cantidad) {
+        return cuenta1;
+    }
+}
